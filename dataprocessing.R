fc6989d67a0d5421baf8400b934229257db7004c
Bryce Huffman <brycehuffman@Bryces-MacBook-Pro.local>
2020-04-07 13:49:57 -0400

    Import Data Processing Steps to GitHub Repo (4/7) Code written on 4/6 by all group members (Thomas, Nathan, Gabby, Zach, Bryce) prior to GitHub suggestion.

---
+++ dataprocessing.R
@@ -0,0 +1,95 @@
+library(tidyr)
+library(WDI)
+library(dplyr)
+library(magrittr)
+library(rvest)
+
+gdp <- WDI(country = "all", indicator = "NY.GDP.PCAP.KD", start = 1950, end = 2018)
+write.csv(gdp, 'gdp.csv', row.names = FALSE)
+
+literacyMale <- WDI(country = "all", indicator = "SE.ADT.LITR.MA.ZS", start = 1950, end = 2018)
+write.csv(literacyMale, 'literacyMale.csv', row.names = FALSE)
+
+literacyFemale <- WDI(country = "all", indicator = "SE.ADT.LITR.FE.ZS", start = 1950, end = 2018)
+write.csv(literacyFemale, 'literacyFemale.csv', row.names = FALSE)
+
+# Pull WHO data from API Query (not functional on 4/6/20)
+mortalityUnder5 <- read.csv(url("https://apps.who.int/gho/athena/api/GHO/MDG_0000000007?format=csv"))
+write.csv(mortalityUnder5, 'mortalityUnder5.csv', row.names = FALSE)
+
+##### Cleaning of World Bank Data ####
+##### GDP, Literacy Male, Literacy Female #####
+
+# Change Column Names
+names(gdp)[3] <- "RealGDP"
+names(literacyFemale)[3] <- "LiteracyFemale"
+names(literacyMale)[3] <- "LiteracyMale"
+names(mortalityUnder5)[5] <- "iso3"
+names(mortalityUnder5)[8] <- "mortalityValue"
+names(mortalityUnder5)[3] <- "year"
+
+## remove rows with NA values
+# set na values to -1
+# remove records that have -1
+
+gdpClean <- gdp %>% mutate(RealGDP = replace(RealGDP, is.na(RealGDP), -1)) %>% filter(RealGDP != -1)
+literacyFemaleClean <- literacyFemale %>% mutate(LiteracyFemale = replace(LiteracyFemale, is.na(LiteracyFemale), -1))%>% filter(LiteracyFemale != -1)
+literacyMaleClean <- literacyMale %>% mutate(LiteracyMale = replace(LiteracyMale, is.na(LiteracyMale), -1)) %>% filter(LiteracyMale != -1)
+mortalityUnder5Clean <- mortalityUnder5 %>% mutate(mortalityValue = replace(mortalityValue, is.na(mortalityValue), -1)) %>% filter(mortalityValue != -1)
+
+## drop columns
+mortalityColDel <- c("GHO", "PUBLISHSTATE", "REGION", "Display.Value", "Low", "High", "Comments")
+mortalityUnder5Clean <- select(mortalityUnder5Clean, -mortalityColDel)
+# create list of country codes for table joining
+
+url <- "https://www.nationsonline.org/oneworld/country_code_list.htm"
+iso_codes <- url %>%
+  read_html() %>%
+  html_nodes(xpath = '//*[@id="CountryCode"]') %>%
+  html_table()
+iso_codes <- iso_codes[[1]][, -1]
+iso_codes <- iso_codes[!apply(iso_codes, 1, function(x){all(x == x[1])}), ]
+names(iso_codes) <- c("Country", "ISO2", "ISO3", "UN")
+head(iso_codes)
+
+# Filter Tables to Countries Only
+# Inner Join with List of Country Codes (if a country and in dataset, keep record)
+
+gdpJoin <- inner_join(iso_codes, gdpClean, by = c("ISO2" = "iso2c"))
+literacyFemaleJoin <- inner_join(iso_codes, literacyFemaleClean, by = c("ISO2" = "iso2c"))
+literacyMaleJoin <- inner_join(iso_codes, literacyMaleClean, by = c("ISO2" = "iso2c"))
+mortalityUnder5Join <- inner_join(iso_codes, mortalityUnder5Clean, by = c("ISO3" = "iso3"))
+
+## Mortality Data by Gender
+
+mortalityFemaleJoin <- filter(mortalityUnder5Join, SEX == "FMLE")
+mortalityMaleJoin<- filter(mortalityUnder5Join, SEX == "MLE")
+mortalityBTSXJoin <- filter(mortalityUnder5Join, SEX == "BTSX")
+
+## Get a cleaned up table that has the years as attributes
+
+literacyMaleFinal <- spread(literacyMaleJoin, year, LiteracyMale)
+literacyFemaleFinal <- spread(literacyFemaleJoin, year, LiteracyFemale)
+gdpFinal <- spread(gdpJoin, year , RealGDP)
+mortalityFemaleFinal <- spread(mortalityFemaleJoin, year, mortalityValue)
+mortalityMaleFinal <- spread(mortalityMaleJoin, year, mortalityValue)
+mortalityBTSXFinal <- spread(mortalityBTSXJoin, year, mortalityValue)
+
+## Export Final Data to CSVs
+
+write.csv(literacyMaleFinal, "literacyMaleFinal.csv", row.names = FALSE)
+write.csv(literacyFemaleFinal, "literacyFemaleFinal.csv", row.names = FALSE)
+write.csv(mortalityBTSXFinal, "mortalityBTSXFinal.csv", row.names = FALSE)
+write.csv(mortalityMaleFinal, "mortalityMaleFinal.csv", row.names = FALSE)
+write.csv(mortalityFemaleFinal, "mortalityFemaleFinal.csv", row.names = FALSE)
+write.csv(gdpFinal, "gdpFinal.csv", row.names = FALSE)
+
+# Import all of the libraries necessary to create a world map 
+# Next, we load in our world data with geograpical coordinates directly from package *ggplot2*. These data 
+# contain geographical coordinates of all countries worldwide, which we'll later need to plot the worldmaps.
+
+library(maps)
+library(ggplot2)
+world_data <- ggplot2::map_data('world')
+world_data <- fortify(world_data)
+head(world_data)
