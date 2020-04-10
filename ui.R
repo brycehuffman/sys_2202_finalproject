5c56146c1ea46b876194739387c97b2f010f6d01
ndb7ab <ndb7ab@virginia.edu>
2020-04-10 15:01:27 -0400

    notes pane, modified well panels

--- ui.R
+++ ui.R
@@ -11,7 +11,7 @@ ui <- fluidPage(
   fluidRow(
     column(6,
       wellPanel(
-        h4("Select"),
+        h4("Select World Map Options"),
           selectInput("map_factor", "Choose a factor for world map visualization.",
             c("Real GDP per Capita, 2010 US Dollars", "Male Literacy Rate, over 15 years old",
               "Female Literacy Rate, over 15 years old", "Infant Mortality Rate per 1000, under 5",
@@ -19,13 +19,24 @@ ui <- fluidPage(
           )
         ),
       wellPanel(
-        h4("Select"),
+        h4("Select Scatterplot Options"),
           selectInput("x_factor", "Choose a factor for the x-axis of the scatter plot",
             c("Real GDP per Capita, 2010 US Dollars", "Male Literacy Rate, over 15 years old",
               "Female Literacy Rate, over 15 years old", "Infant Mortality Rate per 1000, under 5",
               "Female Infant Mortality Rate per 1000, under 5", "Male Infant Mortality Rate per 1000, under 5")
-          )
+          ),
+        selectInput("y_factor", "Choose a factor for the y-axis of the scatter plot",
+                    c("Real GDP per Capita, 2010 US Dollars", "Male Literacy Rate, over 15 years old",
+                      "Female Literacy Rate, over 15 years old", "Infant Mortality Rate per 1000, under 5",
+                      "Female Infant Mortality Rate per 1000, under 5", "Male Infant Mortality Rate per 1000, under 5")
+        )
             
+      ),
+      wellPanel(
+        h4("Notes"),
+        tags$small(paste0(
+          "<><><><><><><this is a place to add notes><><><><><><>"
+        ))
       )
 
     )
