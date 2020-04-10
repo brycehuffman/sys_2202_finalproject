d7a0e75f8aa9d5a1ad5613a90efb334d0256b9b9
Thomas Twomey <tlt7pg@virginia.edu>
2020-04-10 11:58:24 -0700

    Added x-axis drop down

--- ui.R
+++ ui.R
@@ -9,17 +9,29 @@ server <- function(input, output){
 ui <- fluidPage(
   titlePanel("Health and Economic Factors Visualization"),
   fluidRow(
-    column(3,
-           wellPanel(
-             h4("Select"),
-             selectInput("map_factor", "Choose a factor for world map visualization.",
-                         c("Real GDP per Capita, 2010 US Dollars", "Male Literacy Rate, over 15 years old",
-                           "Female Literacy Rate, over 15 years old", "Infant Mortality Rate per 1000, under 5",
-                           "Female Infant Mortality Rate per 1000, under 5", "Male Infant Mortality Rate per 1000, under 5")
-           )
-           )
+    column(6,
+      wellPanel(
+        h4("Select"),
+          selectInput("map_factor", "Choose a factor for world map visualization.",
+            c("Real GDP per Capita, 2010 US Dollars", "Male Literacy Rate, over 15 years old",
+              "Female Literacy Rate, over 15 years old", "Infant Mortality Rate per 1000, under 5",
+              "Female Infant Mortality Rate per 1000, under 5", "Male Infant Mortality Rate per 1000, under 5")
+          )
+        ),
+      wellPanel(
+        h4("Select"),
+          selectInput("x_factor", "Choose a factor for the x-axis of the scatter plot",
+            c("Real GDP per Capita, 2010 US Dollars", "Male Literacy Rate, over 15 years old",
+              "Female Literacy Rate, over 15 years old", "Infant Mortality Rate per 1000, under 5",
+              "Female Infant Mortality Rate per 1000, under 5", "Male Infant Mortality Rate per 1000, under 5")
+          )
+            
+      )
+
+    )
   )
-  )
+  
+  
 )
   
   
