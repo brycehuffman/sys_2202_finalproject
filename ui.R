8fdc93a0e7a61c4d674df3303431202914b25bf3
Bryce Huffman <brycehuffman@Bryces-MacBook-Pro.local>
2020-04-10 14:46:56 -0400

    world map dropdown

--- ui.R
+++ ui.R
@@ -7,15 +7,26 @@ server <- function(input, output){
 }
 
 ui <- fluidPage(
-  titlePanel("Health and Economic Factors Visualization")
-  
-  
-  
-  
-  
-  
-  
-  
+  titlePanel("Health and Economic Factors Visualization"),
+  fluidRow(
+    column(3,
+           wellPanel(
+             h4("Select"),
+             selectInput("map_factor", "Choose a factor for world map visualization.",
+                         c("Real GDP per Capita, 2010 US Dollars", "Male Literacy Rate, over 15 years old",
+                           "Female Literacy Rate, over 15 years old", "Infant Mortality Rate per 1000, under 5",
+                           "Female Infant Mortality Rate per 1000, under 5", "Male Infant Mortality Rate per 1000, under 5")
+           )
+           )
+  )
+  )
 )
+  
+  
+  
+  
+  
+  
+
 
 shinyApp(ui=ui, server=server)
