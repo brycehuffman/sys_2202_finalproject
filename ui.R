090742d66fd16a14b1e8e21d105e51c3fb8c2f41
Bryce Huffman <brycehuffman@Bryces-MacBook-Pro.local>
2020-04-13 15:48:47 -0400

    added year for scatter

--- ui.R
+++ ui.R
@@ -13,12 +13,13 @@ ui <- fluidPage(
               "Female Literacy Rate, over 15 years old", "Infant Mortality Rate per 1000, under 5",
               "Female Infant Mortality Rate per 1000, under 5", "Male Infant Mortality Rate per 1000, under 5")
           ),
-          textInput("Year", "Choose a year between 1950 and 2019:", value = "2018"),
+          textInput("year_map", "Choose a year between 1950 and 2019:", value = "2018"),
         selectInput("scale", "Choose a scale for world map:",
                     c("Normal","Logarithmic"), selected = "Normal")
         ),
       wellPanel(
         h4("Select Scatterplot Options"),
+        textInput("year_scatter", "Choose a year between 1950 and 2019:", value = "2018"),
           selectInput("x_factor", "Choose a factor for the x-axis of the scatter plot",
             c("Real GDP per Capita, 2010 US Dollars", "Male Literacy Rate, over 15 years old",
               "Female Literacy Rate, over 15 years old", "Infant Mortality Rate per 1000, under 5",
