2406b4a78fe1a11a8976850a13dfde5b9acbe37d
ZacheryKey <ztk2qv@virginia.edu>
2020-04-13 16:31:04 -0400

    get the initial lines of code to run the world map that only need to be run one time

---
+++ World_Map_setup.R
@@ -0,0 +1,15 @@
+####---------------------Get the Lat and long coordinates to append to the map of the world---------------####
+library(ggmap)
+
+# Append ISO3 codes to the map.word data to be able to merge long/lat polygon data with the other data frames 
+map.world<-map_data("world")
+map.world$ISO = 0
+map.world$ISO<-countrycode(map.world$region,"country.name","iso3c")
+head(map.world)
+
+# Create dataframes from left joining GDP,Literacy and Mortality to map.world
+map.world_joined<-left_join(map.world,gdpFinal,by=c('ISO'='ISO3'))                   # GDP 
+map.world_joined2<-left_join(map.world,literacyMaleFinal,by=c('ISO'='ISO3'))         # Male Literacy 
+map.world_joined3<-left_join(map.world,literacyFemaleFinal,by=c('ISO'='ISO3'))       # Female Literacy 
+map.world_joined4<-left_join(map.world,mortalityMaleFinal,by=c('ISO'='ISO3'))        # Male Mortality
+map.world_joined5<-left_join(map.world,mortalityFemaleFinal,by=c('ISO'='ISO3'))      # Female Mortality
