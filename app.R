04b4c60d22685575dbda70295739818a4e0f6d26
Bryce Huffman <brycehuffman@Bryces-MacBook-Pro.local>
2020-04-14 00:23:47 -0400

    implemented rewritten code of World Map into UI and Server - FUNCTIONAL!

--- app.R
+++ app.R
@@ -8,6 +8,18 @@ if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.
 if(!require(ggvis)) install.packages("ggvis", repos = "http://cran.us.r-project.org")
 
 
+##### LIBRARIES FOR SERVER: WORLDMAP
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(rvest)) install.packages("rvest", repos = "http://cran.us.r-project.org")
+if(!require(magrittr)) install.packages("magrittr", repos = "http://cran.us.r-project.org")
+if(!require(ggmap)) install.packages("ggmap", repos = "http://cran.us.r-project.org")
+if(!require(stringr)) install.packages("stringr", repos = "http://cran.us.r-project.org")
+if(!require(countrycode)) install.packages("countrycode", repos = "http://cran.us.r-project.org")
+if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+if(!require(ggiraph)) install.packages("ggiraph", repos = "http://cran.us.r-project.org") # interactive labels for plot
+if(!require(RColorBrewer)) install.packages("RColorBrewer", repos = "http://cran.us.r-project.org") # interactive labels for plot
+
+
 ##### DATA PROCESSING (AS OF 4/13) #####
 
 if(!require(tidyr)) install.packages("tidyr", repos = "http://cran.us.r-project.org")
@@ -149,7 +161,8 @@ ui <- fluidPage(
         )
       )),
       column(9, wellPanel( 
-        h4("World Map Visualization")),
+        h4("World Map Visualization"),
+        plotOutput("mapPlot")),
         wellPanel(
           h4("Scatterplot Visualization"), 
           ggvisOutput("scatter1")
@@ -158,6 +171,192 @@ ui <- fluidPage(
 
 
 ##### FUNCTIONS #####
+
+## Functions for World Map
+
+setDataSource <- function(name){
+  # returns correct data source based on given name
+  if(name=="Female Mortality"){
+    return(map.world_joined5)
+  }else{
+    if(name=="Male Mortality"){
+      return(map.world_joined4)
+    }else{
+      if(name=="GDP"){
+        return(map.world_joined)
+      }else{
+        if(name=="Female Literacy"){
+          return(map.world_joined3)
+        }else{
+          if(name=="Male Literacy"){
+            return(map.world_joined2)
+          }
+        }
+      }
+    }
+  }
+}
+
+
+setContributor <- function(name){
+  # returns correct contributor info based on given name
+  if(name=="Female Mortality"){
+    return("World Health Organization") 
+  }else{
+    if(name=="Male Mortality"){
+      return("World Health Organization")
+    }else{
+      if(name=="GDP"){
+        return("World Bank")
+      }else{
+        if(name=="Female Literacy"){
+          return("World Bank")
+        }else{
+          if(name=="Male Literacy"){
+            return("World Bank")
+          }
+        }
+      }
+    }
+  }
+}
+
+
+setData_Units <- function(name){
+  # returns correct data units info based on name
+  if(name=="Female Mortality"){
+    return("number of female children under 5 who died per 1000 live births")
+  }else{
+    if(name=="Male Mortality"){
+      return("number of male children under 5 who died per 1000 live births")
+    }else{
+      if(name=="GDP"){
+        return("GDP in US dollars per capita")
+      }else{
+        if(name=="Female Literacy"){
+          return("percent of females who are literate")
+        }else{
+          if(name=="Male Literacy"){
+            return("percent of males who are literate")
+          }
+        }
+      }
+    }
+  }
+}
+
+createWorldMap <- function(name, year_input){
+  DataSource = setDataSource(name)
+  Contributor = setContributor(name)
+  data_units = setData_Units(name)
+  
+  # Get column corresponding to user specified date and create a boolean column for missing values
+  a = as.numeric(which(names(DataSource)==year_input))                        # get the column corresponding to user specified date 
+  DataSource$missingD2 = with(DataSource, is.na(DataSource[,a]))              # see which rows are missing data for the specified year
+  
+  # Apply a logarithmic scale to normalize the color distribution of the world map for GDP
+  Selected = DataSource[,a]                                                             # Data for choosen attribute for user selected year
+  DataSource$factors = Selected/(max(na.omit(Selected)))                                # Get a ratio for GDP values from max GDP for that year
+  DataSource$lnfactors = with(DataSource, pmin(log(1.5+DataSource$factors),.6))         # Apply ln transform to normalize colors on graph
+  
+  # CHOOSE WHAT TYPE OF COLOR SCHEMA YOU WANT (regular or log-scale) based on 
+  # type of data set and "spread" of the data altogether
+  
+  if(name=="GDP"){
+    filler = DataSource$lnfactors
+  }else{
+    filler = DataSource$factors}
+  
+  # See what Type of plot you are using -- (Could be replaced by just a name check but who knows if you'll need it later)
+  ln = na.omit(ifelse(filler==DataSource$factors,"linear","logarithmic"))
+  plotType = ln[1]
+  
+  # Calculate the scale to customize the legend used in the Map
+  max_n = max(na.omit(filler))
+  min_n = min(na.omit(filler))
+  range_n = max_n-min_n
+  size_ticks = range_n/6
+  
+  # Get the units of the tick marks on the legend set
+  tick0 = min_n                                                
+  tick1 = tick0 + size_ticks
+  tick2 = tick1 + size_ticks
+  tick3 = tick2 + size_ticks
+  tick4 = tick3 + size_ticks
+  tick5 = tick4 + size_ticks
+  tick6 = max_n
+  
+  # Get quartiles for the legend in linear and log format
+  if(plotType=="linear"){
+    max_value = as.integer(max(na.omit(DataSource[,a])))           
+    min_value = as.integer(min(na.omit(DataSource[,a])))
+    
+    q1_value = as.integer((1*(max_value-min_value)/6)+min_value)
+    q2_value = as.integer((2*(max_value-min_value)/6)+min_value)
+    q3_value = as.integer((3*(max_value-min_value)/6)+min_value)
+    q4_value = as.integer((4*(max_value-min_value)/6)+min_value)
+    q5_value = as.integer((5*(max_value-min_value)/6)+min_value)
+  }
+  
+  if(plotType=="logarithmic"){
+    max_value = as.integer(max(na.omit(DataSource[,a]))) 
+    max_ln = log(max_value)
+    min_value = as.integer(min(na.omit(DataSource[,a])))
+    min_ln = log(min_value)
+    diff = max_ln - min_ln
+    
+    q1_value_ln = ((1*(diff)/6)+min_ln)
+    q2_value_ln = ((2*(diff)/6)+min_ln)
+    q3_value_ln = ((3*(diff)/6)+min_ln)
+    q4_value_ln = ((4*(diff)/6)+min_ln)
+    q5_value_ln = ((5*(diff)/6)+min_ln)
+    
+    q1_value = round(exp(q1_value_ln),-2)
+    q2_value = round(exp(q2_value_ln),-2)
+    q3_value = round(exp(q3_value_ln),-3)
+    q4_value = round(exp(q4_value_ln),-3)
+    q5_value = round(exp(q5_value_ln),-3)
+    
+    max_value = round(max_value,-3)
+    min_value = round(min_value,-2)
+  }
+  
+  #Set labels for the legend ticks
+  lab0 = min_value 
+  lab1 = q1_value
+  lab2 = q2_value
+  lab3 = q3_value
+  lab4 = q4_value
+  lab5 = q5_value
+  lab6 = max_value
+  
+  
+  # Get the value to be shown when you hover over the map with the mouse
+  DataSource$Country = DataSource$region
+  DataSource$Country = paste(DataSource$Country,":",as.integer(DataSource[,a]))
+  
+  myMap2<-ggplot() +
+    geom_polygon_interactive(data = DataSource, aes(x = long, y = lat, group = group, fill = filler, tooltip = Country, data_id = region)) +
+    labs(title = paste(Start_year,name,sep = " "), subtitle = paste(data_units,"with colors displayed on a ",plotType,"scale."), caption = paste("source: ",Contributor)) +
+    scale_fill_gradientn(name=name,
+                         colours = brewer.pal(5, "RdYlBu"), 
+                         na.value = 'white',
+                         breaks=c(tick0,tick1,tick2,tick3,tick4,tick5,tick6),
+                         labels=c(lab0,lab1,lab2,lab3,lab4,lab5,lab6)) +
+    theme(text = element_text(color = "#FFFFFF")
+          ,panel.background = element_rect(fill = "#444444")
+          ,plot.background = element_rect(fill = "#444444")
+          ,panel.grid = element_blank()
+          ,plot.title = element_text(size = 30)
+          ,plot.subtitle = element_text(size = 10)
+          ,axis.text = element_blank()
+          ,axis.title = element_blank()
+          ,axis.ticks = element_blank()
+          ,legend.position = "right"
+          ,legend.background = element_rect(fill = "#444444")
+    )
+  return(myMap2)
+}
 
 ## Functions for scatterplot
 
@@ -186,7 +385,28 @@ createScatterplot <- function(data){
 ##### SERVER (from scratch) #####
 
 server <- function(input, output){
+  
+## Reactive World Map
+  
+  # reactive world map year
+  Start_year = reactive({
+    format(input$year_map)
+  })
+  
+  # setup reactive variable for map factor in UI
+  name <- reactive({
+    if ("Real GDP per Capita, 2010 US Dollars" %in% input$map_factor) return("GDP")
+    if ("Male Literacy Rate, over 15 years old" %in% input$map_factor) return("Male Literacy")
+    if ("Female Literacy Rate, over 15 years old" %in% input$map_factor) return("Female Literacy")
+    if ("Infant Mortality Rate per 1000, under 5" %in% input$map_factor) return("Overall Mortality") # NEEDS IMPLEMENTED
+    if ("Female Infant Mortality Rate per 1000, under 5" %in% input$map_factor) return("Female Mortality")
+    if ("Male Infant Mortality Rate per 1000, under 5" %in% input$map_factor) return("Male Mortality")
+  })
 
+  # create World Map Plot Output
+  output$mapPlot <- renderPlot({createWorldMap(name(), Start_year())})
+  
+  
 ## Reactive Scatterplot
   
   # setup reactive variable for x factor in UI
